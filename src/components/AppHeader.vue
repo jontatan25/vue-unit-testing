c1a0fa1f91ab713f0dc3f03329a9038030d96174
Jonathan Sarmiento <jhonn.photo@gmail.com>
2023-03-29 04:16:06 -0500

    chore/creating base project

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      loggedIn: false,
+    };
+  },
+};
+</script>
