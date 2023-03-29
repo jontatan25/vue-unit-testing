031ab4ccd06aff41f4f079fc244bacf6cb2dc57f
Jonathan Sarmiento <jhonn.photo@gmail.com>
2023-03-29 08:23:43 -0500

    feat/ Emit events testing implemented

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="name" />
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      name: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$emit("formSubmitted", { name: this.name });
+    },
+  },
+};
+</script>
